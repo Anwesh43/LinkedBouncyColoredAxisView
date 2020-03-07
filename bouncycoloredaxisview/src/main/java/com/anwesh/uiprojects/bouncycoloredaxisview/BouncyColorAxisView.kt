52b95cde6445814154ea00b261c8571fa7bb24ee
Anwesh43 <anweshthecool0@gmail.com>
2020-03-07 20:05:23 +0530

    adding constants and libraries

--- bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
+++ bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.bouncycoloredaxisview
  * Created by anweshmishra on 07/03/20.
  */
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val lines : Int = 2
+val parts : Int = 3
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 25
+val scGap : Float = 0.2f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<String> = arrayOf("#4CAF50", "#673AB7", "#f44336", "#01579B", "#009688")

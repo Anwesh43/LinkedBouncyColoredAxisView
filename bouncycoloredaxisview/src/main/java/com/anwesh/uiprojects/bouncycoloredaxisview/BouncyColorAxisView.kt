8a6c0d2af62a17a4d52b1ebde69fa3b2c7368980
Anwesh43 <anweshthecool0@gmail.com>
2020-03-07 20:15:45 +0530

    adding Canvas extension methods

--- bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
+++ bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
@@ -24,3 +24,37 @@ val colors : Array<String> = arrayOf("#4CAF50", "#673AB7", "#f44336", "#01579B",
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBouncyColorAxis(i : Int, scale : Float, size : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val si : Float = 1f - 2 * i
+    val sc2i : Float = sc2.divideScale(i, lines)
+    val sc3i : Float = sc3.divideScale(i, lines)
+    save()
+    scale(1f, si)
+    translate(0f, (h / 2) * sc3i)
+    rotate(90f * sc2i)
+    drawLine(0f, 0f, size * sc1, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBouncyColorAxises(scale : Float, size : Float, h : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawBouncyColorAxis(j, scale, size, h, paint)
+    }
+}
+
+fun Canvas.drawBCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2)
+    drawBouncyColorAxises(scale, size, h, paint)
+    restore()
+}

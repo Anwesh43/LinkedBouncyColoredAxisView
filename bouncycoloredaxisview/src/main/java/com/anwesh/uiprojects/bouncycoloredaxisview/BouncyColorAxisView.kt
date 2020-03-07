ea999161ae3bf3c5347c6f941267cbfa4209b92e
Anwesh43 <anweshthecool0@gmail.com>
2020-03-07 20:17:42 +0530

    adding BouncyColorAxisView class

--- bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
+++ bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBCANode(i : Int, scale : Float, paint : Paint) {
     drawBouncyColorAxises(scale, size, h, paint)
     restore()
 }
+
+class BouncyColorAxisView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

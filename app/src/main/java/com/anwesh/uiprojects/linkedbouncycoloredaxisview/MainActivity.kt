efd4420b2245bde4d614f5f79b6da170ba7f9e60
Anwesh43 <anweshthecool0@gmail.com>
2020-03-07 20:36:11 +0530

    adding app in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbouncycoloredaxisview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbouncycoloredaxisview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbouncycoloredaxisview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.bouncycoloredaxisview.BouncyColorAxisView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BouncyColorAxisView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

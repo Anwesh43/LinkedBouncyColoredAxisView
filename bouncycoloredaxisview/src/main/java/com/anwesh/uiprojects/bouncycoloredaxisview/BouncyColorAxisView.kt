7654597bca51e86ae567c80afcebe4e2b25259b3
Anwesh43 <anweshthecool0@gmail.com>
2020-03-07 20:06:32 +0530

    adding Int inverse methods

--- bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
+++ bouncycoloredaxisview/src/main/java/com/anwesh/uiprojects/bouncycoloredaxisview/BouncyColorAxisView.kt
@@ -20,3 +20,7 @@ val delay : Long = 25
 val scGap : Float = 0.2f / parts
 val backColor : Int = Color.parseColor("#BDBDBD")
 val colors : Array<String> = arrayOf("#4CAF50", "#673AB7", "#f44336", "#01579B", "#009688")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
